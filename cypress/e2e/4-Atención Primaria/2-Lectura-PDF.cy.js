7329c59015f38baae27bba6a14e475a12ef015e5
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-11 20:36:51 -0300

    Lectura-PDF

---
+++ cypress/e2e/4-Atención Primaria/2-Lectura-PDF.cy.js
@@ -0,0 +1,38 @@
+const path = require('path'); // Asegúrate de tener esta línea al principio
+
+describe('Test de extracción de texto de PDF', () => {
+    it('Extrae texto de un archivo PDF y valida', () => {
+        // Usa la ruta absoluta directamente o asegúrate de que downloadDirectory esté configurado
+        const pdfFilePath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\aadeudacontadosellados2_impl.pdf';
+
+        cy.task('parsePdf', { filePath: pdfFilePath })
+        .then((extractedText) => {
+            // Normalizar el texto extraído
+            const normalizedText = extractedText.replace(/\s+/g, ' ').trim();
+
+            cy.log(normalizedText);
+
+            // Obtener la fecha de hoy en formato dd/mm/yy
+            const today = new Date();
+            const day = String(today.getDate()).padStart(2, '0');
+            const month = String(today.getMonth() + 1).padStart(2, '0');
+            const year = String(today.getFullYear()).slice(-2, '0');
+            const formattedDate = `${day}/${month}/${year}`;
+
+            // Validaciones con texto normalizado
+            expect(normalizedText).to.include('Usuario: dsimoncini');
+            expect(normalizedText).to.include(`${formattedDate}`);
+            expect(normalizedText).to.include('CARRION MICAELA DANIELA');
+            expect(normalizedText).to.include('46860');
+            expect(normalizedText).to.include(`${formattedDate}`);
+            expect(normalizedText).to.include('COMERCIO');
+            expect(normalizedText).to.include('Municipalidad de Godoy Cruz');
+            expect(normalizedText).to.include('Comprobante');
+            expect(normalizedText).to.include('Este comprobante se considerará pago, solo con');
+            expect(normalizedText).to.include('el ticket de caja correspondiente.');
+            expect(normalizedText).to.include('TOTAL');
+            expect(normalizedText).to.include('INTERESES');
+            expect(normalizedText).to.include('CÓDIGO');
+        });
+    });
+});
