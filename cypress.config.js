d2051ddad70b38e13a58b3e1fb45d04ea83bcd68
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-12 14:53:36 -0300

    Ruta Descarga

--- cypress.config.js
+++ cypress.config.js
@@ -78,7 +78,7 @@ module.exports = defineConfig({
     },
     baseUrl: 'https://test.elinpar.com',
     env: {
-      downloadDirectory: 'C:\\Users\\Lmarquez\\Downloads',
+      downloadDirectory: 'C:\\Users\\Lmarquez\\Downloads\\GC',
     }
   }
 });
