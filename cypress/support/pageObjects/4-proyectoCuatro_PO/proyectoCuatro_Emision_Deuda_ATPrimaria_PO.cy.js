73f579b27cbd753098f2449abf41111c7e20bcdc
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-11 21:51:53 -0300

    Achivo PDF

--- cypress/support/pageObjects/4-proyectoCuatro_PO/proyectoCuatro_Emision_Deuda_ATPrimaria_PO.cy.js
+++ cypress/support/pageObjects/4-proyectoCuatro_PO/proyectoCuatro_Emision_Deuda_ATPrimaria_PO.cy.js
@@ -48,13 +48,13 @@ class ProyectoCuatro_Emision_Deuda_ATPrimaria_Po{
     cy.wait(5000)
 
     cy.iframe().xpath("//input[@id='IMPRIMIRCONTADO']").invoke('click')
-    cy.wait(4000)
+    cy.wait(8000)
     
       }
            
       SeccionTres(t){
         let tiempo=t
-       cy.wait(4000)
+       cy.wait(6000)
         cy.get('#USERNAMEINITIALS_MPAGE').should('be.visible').click({force: true})
         cy.get('#SIGNOUT_MPAGE').should('be.visible').click({force: true})
 
