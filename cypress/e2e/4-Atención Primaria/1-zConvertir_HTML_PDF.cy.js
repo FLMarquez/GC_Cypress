4648d6019efa94597be33698e4e9c1fb360dfc3e
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-13 12:58:57 -0300

    Modif cypress.config

--- cypress/e2e/4-Atención Primaria/1-zConvertir_HTML_PDF.cy.js
+++ cypress/e2e/4-Atención Primaria/1-zConvertir_HTML_PDF.cy.js
@@ -1,20 +1,25 @@
 describe('Pruebas de conversión de HTML a PDF', () => {
     it('Convierte el archivo HTML descargado a PDF', () => {
+      const inputPath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\downloads.htm';
+      const outputPath = 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\aadeudacontadosellados2_impl.pdf';
+  
       // Verifica si el archivo HTML existe antes de intentar convertirlo
-      cy.readFile('C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\downloads.htm')
-        .then(() => {
+      cy.readFile(inputPath, { timeout: 10000 }).then((content) => {
+        if (content) {
           // Llama a la tarea de conversión
           cy.task('convertHtmlToPdf', {
-            input: 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\downloads.htm',
-            output: 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\aadeudacontadosellados2_impl.pdf'
+            input: inputPath,
+            output: outputPath
           }).then(() => {
             cy.log('HTML convertido a PDF exitosamente.');
           });
-        })
-        .catch((err) => {
-          // Muestra un mensaje si el archivo HTML no se encuentra
-          cy.log('Error al leer el archivo HTML:', err.message);
-        });
+        } else {
+          cy.log('El archivo HTML está vacío o no se pudo leer correctamente.');
+        }
+      }).catch((err) => {
+        cy.log('Error al leer el archivo HTML:', err.message);
+        throw err; // Opcional, para marcar la prueba como fallida si ocurre un error
+      });
     });
   });
   
