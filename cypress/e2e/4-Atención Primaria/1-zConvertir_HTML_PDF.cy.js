b0ab9882d2f476391fec2569216db05abb24c589
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-13 12:39:24 -0300

    Modif cypress.config

--- cypress/e2e/4-Atención Primaria/1-zConvertir_HTML_PDF.cy.js
+++ cypress/e2e/4-Atención Primaria/1-zConvertir_HTML_PDF.cy.js
@@ -1,22 +1,21 @@
 describe('Pruebas de conversión de HTML a PDF', () => {
-    it('Descarga y convierte el archivo HTML a PDF', () => {
-      cy.visit('https://test.elinpar.com/descargar-archivo'); // Cambia esto a la URL que inicia la descarga
-  
-      // Espera a que el archivo HTML se descargue
-      cy.wait(5000); // Ajusta el tiempo de espera según sea necesario
-  
-      const htmlPath = 'C:/ProgramData/Jenkins/.jenkins/workspace/GC_Cypress_Pipeline/cypress/downloads/downloads.htm';
-      const pdfPath = 'C:/ProgramData/Jenkins/.jenkins/workspace/GC_Cypress_Pipeline/cypress/downloads/aadeudacontadosellados2_impl.pdf';
-  
-      // Convierte el archivo HTML a PDF usando la tarea de Cypress
-      cy.task('convertHtmlToPdf', { input: htmlPath, output: pdfPath }).then((error) => {
-        if (error) {
-          throw new Error('Error al convertir HTML a PDF: ' + error);
-        }
-  
-        // Verifica que el PDF se haya creado correctamente
-        cy.readFile(pdfPath).should('exist');
-      });
+    it('Convierte el archivo HTML descargado a PDF', () => {
+      // Verifica si el archivo HTML existe antes de intentar convertirlo
+      cy.readFile('C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\downloads.htm')
+        .then(() => {
+          // Llama a la tarea de conversión
+          cy.task('convertHtmlToPdf', {
+            input: 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\downloads.htm',
+            output: 'C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GC_Cypress_Pipeline\\cypress\\downloads\\aadeudacontadosellados2_impl.pdf'
+          }).then(() => {
+            cy.log('HTML convertido a PDF exitosamente.');
+          });
+        })
+        .catch((err) => {
+          // Muestra un mensaje si el archivo HTML no se encuentra
+          cy.log('Error al leer el archivo HTML:', err.message);
+        });
     });
   });
+  
   
