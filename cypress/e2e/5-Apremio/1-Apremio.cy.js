598b45fbdbad1252cd294832342948e380528d70
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-13 16:08:59 -0300

    Cp Apremio

---
+++ cypress/e2e/5-Apremio/1-Apremio.cy.js
@@ -0,0 +1,32 @@
+//NumeroObjeto: Dato Prueba ----- 46860
+
+import ProyectoCinco_Apremio_PO from '../../support/pageObjects/5-proyectoCinco_PO/proyectroCinco_Apremio_PO.cy'
+
+/// <reference types='cypress' />
+require('cypress-xpath')
+
+
+describe('Apremio', () => {
+
+    const master=new ProyectoCinco_Apremio_PO()
+    
+    master.visitHome()
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from failing the test
+        if (err.message.includes('Package is not defined')) {
+          return false;
+        }
+        // otherwise, return true
+        return false;
+    
+})
+
+it('Apremio', () => {        
+    master.SeccionUno("dsimoncini","dsimoncini",1000) 
+    master.SeccionDos("COMERCIO",1000) 
+    master.SeccionTres(1000)  
+       
+});
+
+});
---
+++ cypress/support/pageObjects/5-proyectoCinco_PO/proyectroCinco_Apremio_PO.cy.js
@@ -0,0 +1,55 @@
+import 'cypress-iframe';
+require('cypress-downloadfile/lib/downloadFileCommand');
+
+class ProyectoCinco_Apremio_PO{
+
+    visitHome() {
+        let tiempo = 1000;
+        beforeEach(() => {
+          cy.visit('https://gcdigital.godoycruz.gob.ar/K2BGAM/servlet/com.k2bgam.k2blogin');
+          cy.wait(tiempo);
+        });
+      }
+    
+      SeccionUno(usuario, contrasena, t) {
+        let tiempo = t;
+        if (usuario !== "") {
+          cy.xpath("//input[contains(@id,'vUSERNAME')]").should("be.visible", { timeout: 5000 }).type(usuario);
+        }
+        if (contrasena !== "") {
+          cy.xpath("//input[contains(@id,'vUSERPASSWORD')]").should("be.visible", { timeout: 5000 }).type(contrasena);
+        }
+        cy.wait(tiempo);
+        cy.xpath("//input[contains(@id,'LOGIN')]").should("be.visible", { timeout: 5000 }).click();
+        cy.wait(tiempo);
+      }
+    
+      SeccionDos(objeto, t) {
+        let tiempo = t;
+       
+    cy.get('[name="BTNTOGGLEMENU_MPAGE"]').should("be.visible", { timeout: 5000 }).click()
+    cy.get('span.sidebar-nav-item').should("be.visible").contains('Tributario Faro').click({ force: true })         
+    cy.xpath("(//a[contains(.,'Apremio')])[2]").invoke('show').click({ force: true })         
+    cy.xpath("//a[contains(.,'Administración Apremios')]").invoke('show').click({ force: true })
+    cy.wait(6000)
+
+    cy.frameLoaded({ index: 0 })
+    cy.iframe().find('#vOTPID').select(objeto);
+    cy.iframe().find('#BUTTON1').click();
+    
+        
+      }
+           
+      SeccionTres(t){
+        let tiempo=t
+       cy.wait(1000)
+        cy.get('#USERNAMEINITIALS_MPAGE').should('be.visible').click({force: true})
+        cy.get('#SIGNOUT_MPAGE').should('be.visible').click({force: true})
+
+    }
+
+        
+      
+    }//final
+
+export default ProyectoCinco_Apremio_PO
