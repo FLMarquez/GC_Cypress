f280c878861fe7df1838f44337567231b8c8c225
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-13 18:53:04 -0300

    Cp Emision Tasas

---
+++ cypress/e2e/7-Emision_Tasas/1-Emision_Tasas.cy.js
@@ -0,0 +1,33 @@
+//NumeroObjeto: Dato Prueba ----- 46860
+
+import ProyectoSiete_Emisione_Tasas_PO from '../../support/pageObjects/7-proyectoSiete_PO/proyectoSiete_Emision_Tasas_PO.cy'
+
+/// <reference types='cypress' />
+require('cypress-xpath')
+
+
+describe('Emision de Tasa', () => {
+
+    const master=new ProyectoSiete_Emisione_Tasas_PO()
+    
+    master.visitHome()
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from failing the test
+        if (err.message.includes('Package is not defined')) {
+          return false;
+        }
+        // otherwise, return true
+        return false;
+    
+})
+
+it('Emision de Tasa', () => {        
+    master.SeccionUno("dsimoncini","dsimoncini",1000) 
+    master.SeccionDos("20276701429","502104",1000) 
+    master.SeccionTres(1000)  
+    master.SeccionCuatro(1000) 
+       
+});
+
+});
---
+++ cypress/support/pageObjects/7-proyectoSiete_PO/proyectoSiete_Emision_Tasas_PO.cy.js
@@ -0,0 +1,93 @@
+import 'cypress-iframe';
+require('cypress-downloadfile/lib/downloadFileCommand');
+
+class proyectoSiete_Emision_Tasas_PO{
+
+    visitHome() {
+        let tiempo = 1000;
+        beforeEach(() => {
+          cy.visit('https://gcdigital.godoycruz.gob.ar/K2BGAM/servlet/com.k2bgam.k2blogin');
+          cy.wait(tiempo);
+        });
+      }
+    
+      SeccionUno(usuario, contrasena, t) {
+        let tiempo = t;
+        if (usuario !== "") {
+          cy.xpath("//input[contains(@id,'vUSERNAME')]").should("be.visible", { timeout: 5000 }).type(usuario);
+        }
+        if (contrasena !== "") {
+          cy.xpath("//input[contains(@id,'vUSERPASSWORD')]").should("be.visible", { timeout: 5000 }).type(contrasena);
+        }
+        cy.wait(tiempo);
+        cy.xpath("//input[contains(@id,'LOGIN')]").should("be.visible", { timeout: 5000 }).click();
+        cy.wait(tiempo);
+      }
+    
+      SeccionDos(identificacion_tributaria,tasa, t) {
+        let tiempo = t;
+       
+    cy.get('[name="BTNTOGGLEMENU_MPAGE"]').should("be.visible", { timeout: 5000 }).click()
+    cy.get('span.sidebar-nav-item').should("be.visible").contains('Tributario Faro').click({ force: true })         
+    cy.xpath("(//a[contains(.,'Tasas Varias')])[2]").invoke('show').click({ force: true })         
+    cy.xpath("//a[contains(.,'Generar Tasas Varias')]").invoke('show').click({ force: true })
+    cy.wait(2000)
+
+
+    
+    cy.get('iframe[name="EMBPAGEM"]').its('0.contentDocument.body').should('not.be.empty').then(cy.wrap).within(() => {
+    //cy.frameLoaded({ index: 0 })
+    cy.get('#vDOCUMENTO').invoke('show').clear();
+    cy.wait(tiempo)
+    cy.get('#vDOCUMENTO').invoke('show').type(identificacion_tributaria);
+    cy.wait(tiempo)
+    cy.xpath("//label[contains(.,'Razón Social / Apellido')]").should('be.visible').click({force: true})
+    cy.wait(tiempo)
+    cy.get('#vCMCCODIGO').should("be.visible").click().type(tasa);
+    cy.wait(tiempo);
+    cy.get('#vDETALLEINSTRUMENTO').should("be.visible").click();
+    cy.wait(tiempo);
+    cy.get('#AGREGAR').should("be.visible").click();
+    cy.wait(tiempo);
+    cy.get('#CALCULAR').should("be.visible").click();
+    cy.wait(tiempo);
+    cy.get('#GENERARTASA').should("be.visible").click();
+    cy.wait(tiempo);
+       
+  
+    });
+    
+    
+        
+      }
+
+      SeccionTres(t){
+        let tiempo=t
+      cy.get('iframe[name="EMBPAGEM"]').its('0.contentDocument.body').should('not.be.empty').then(cy.wrap).within(() => {
+        // Seleccionar el iframe hijo 'gxp1_ifrm'
+        cy.get('iframe[id="gxp0_ifrm"]').its('0.contentDocument.body').should('not.be.empty').then(cy.wrap).within(() => {
+                  cy.get('#TEXTBLOCK').invoke('show').and('contain.text', 'Tasas Generadas Correctamente');
+                  cy.get('#BUTTON1').should('be.visible').click({force: true})
+                  cy.wait(tiempo)
+    }); 
+    
+        });
+      }
+
+       
+    
+           
+      SeccionCuatro(t){
+        let tiempo=t
+       cy.wait(1000)
+        cy.get('#USERNAMEINITIALS_MPAGE').should('be.visible').click({force: true})
+        cy.get('#SIGNOUT_MPAGE').should('be.visible').click({force: true})
+
+    }
+  
+  
+        
+      
+    }//final
+
+export default proyectoSiete_Emision_Tasas_PO
