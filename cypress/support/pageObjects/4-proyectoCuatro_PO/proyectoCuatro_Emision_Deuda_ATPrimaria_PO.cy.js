445733e3b71655789e219d496fa591cf3bbbfb8d
Lucas <flucas.marquez@hotmail.com.ar>
2024-09-14 17:54:12 -0300

    Se modifica cypress.config

--- cypress/support/pageObjects/4-proyectoCuatro_PO/proyectoCuatro_Emision_Deuda_ATPrimaria_PO.cy.js
+++ cypress/support/pageObjects/4-proyectoCuatro_PO/proyectoCuatro_Emision_Deuda_ATPrimaria_PO.cy.js
@@ -44,7 +44,7 @@ class ProyectoCuatro_Emision_Deuda_ATPrimaria_Po {
     cy.wait(5000);
 
     cy.iframe().xpath("(//label[contains(@for,'GRIDOBLIGACIONES')])[1]").should('be.visible').click({ force: true });
-    cy.wait(5000);
+    cy.wait(50000);
 
     cy.iframe().xpath("//input[@id='IMPRIMIRCONTADO']").invoke('click');
     cy.wait(30000); // Espera 10 segundos para asegurar que el PDF se descargue
