567a5685f9a5a0de5586de16e38d57c69f9a2d82
Lucas <flucas.marquez@hotmail.com.ar>
2024-10-14 14:15:29 -0300

    Se modifica emision deuda atprimaria

--- cypress/support/pageObjects/4-proyectoCuatro_PO/proyectoCuatro_Emision_Deuda_ATPrimaria_PO.cy.js
+++ cypress/support/pageObjects/4-proyectoCuatro_PO/proyectoCuatro_Emision_Deuda_ATPrimaria_PO.cy.js
@@ -6,7 +6,18 @@ class ProyectoCuatro_Emision_Deuda_ATPrimaria_Po {
   visitHome() {
     let tiempo = 1000;
     beforeEach(() => {
-      cy.visit('https://gcdigital.godoycruz.gob.ar/K2BGAM/servlet/com.k2bgam.k2blogin');
+      cy.visit('https://gcdigital.godoycruz.gob.ar/K2BGAM/servlet/com.k2bgam.k2blogin', {
+        timeout: 300000, // Tiempo máximo de espera en milisegundos
+        onBeforeLoad: (win) => {
+          // Acciones antes de que se cargue la página
+          console.log('La página está a punto de cargarse');
+        },
+        onLoad: (win) => {
+          // Acciones para cuando la página se carga completamente
+          console.log('La página se ha cargado completamente');
+        },
+        waitForLoad: false, // No esperar a que se cargue completamente
+      });
       cy.wait(tiempo);
     });
   }
@@ -46,7 +57,7 @@ class ProyectoCuatro_Emision_Deuda_ATPrimaria_Po {
     cy.iframe().xpath("(//label[contains(@for,'GRIDOBLIGACIONES')])[1]").should('be.visible').click({ force: true });
     cy.wait(2500);
 
-    cy.iframe().xpath("//input[@id='IMPRIMIRCONTADO']").invoke('click');
+    cy.iframe().xpath("//input[@id='IMPRIMIRCONTADO']").should('be.visible').click();
     cy.wait(9000); // Espera 10 segundos para asegurar que el PDF se descargue
 
   }
